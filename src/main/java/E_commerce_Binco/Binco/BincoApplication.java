281f07d0d5d0b1686ddb10dac8900be18f988221
JulioSAlencar <jsalencar24@gmail.com>
2025-01-02 20:56:05 -0300

    first commit

---
+++ src/main/java/E_commerce_Binco/Binco/BincoApplication.java
@@ -0,0 +1,13 @@
+package E_commerce_Binco.Binco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BincoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BincoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/E_commerce_Binco/Binco/BincoApplicationTests.java
@@ -0,0 +1,13 @@
+package E_commerce_Binco.Binco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BincoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
